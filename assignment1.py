ab99376b505a860c59298de356165b395f044ffc
Jacob-Kaczmarek <jkaczmarek@unomaha.edu>
2025-02-15 12:32:43 -0600

    Attempt 3

--- assignment1.py
+++ assignment1.py
@@ -13,16 +13,15 @@ train_data_prophet = train_data.reset_index().rename(columns={"Timestamp": "ds",
 model = Prophet()
 modelFit = model.fit(train_data_prophet)
 
-test_data_url = "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv"
-test_data = pd.read_csv(test_data_url, parse_dates=["Timestamp"], index_col="Timestamp")
+test_data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv")
+test_data['Timestamp'] = pd.to_datetime(test_data['Timestamp'])
+future = test_data.rename(columns={'Timestamp': 'ds'})
 
-future = pd.DataFrame({"ds": pd.date_range(start=test_data.index.min(), periods=744, freq="H")})
-
-
-forecast = model.predict(future)
+pred = modelFit.predict(future)
 
 
-pred = forecast[["ds", "yhat"]].rename(columns={"yhat": "predicted_trips"})
-
+pred = pred[['ds', 'yhat']]
+pred['yhat'] = pred['yhat'].astype(int)
+pred = pred.set_index('ds')
 pred
 
