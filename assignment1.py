ed4376048efaaa98a9c5c909118ed24bcfb1c95e
Jacob-Kaczmarek <jkaczmarek@unomaha.edu>
2025-02-15 12:20:09 -0600

    attempt 2

--- assignment1.py
+++ assignment1.py
@@ -1,2 +1,28 @@
-y = 8 * 3 
-y
+
+import pandas as pd
+from prophet import Prophet
+
+
+train_data_url = "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv"
+train_data = pd.read_csv(train_data_url, parse_dates=["Timestamp"], index_col="Timestamp")
+
+
+train_data_prophet = train_data.reset_index().rename(columns={"Timestamp": "ds", "trips": "y"})
+
+
+model = Prophet()
+modelFit = model.fit(train_data_prophet)
+
+test_data_url = "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv"
+test_data = pd.read_csv(test_data_url, parse_dates=["Timestamp"], index_col="Timestamp")
+
+future = pd.DataFrame({"ds": pd.date_range(start=test_data.index.min(), periods=744, freq="H")})
+
+
+forecast = model.predict(future)
+
+
+pred = forecast[["ds", "yhat"]].rename(columns={"yhat": "predicted_trips"})
+
+pred
+
